a22bbd68ded03ce6d7db6df3e29852543c34dd21
shafe <alam.shafee@gmail.com>
2026-01-11 17:16:27 +0600

    if else statement example

---
+++ src/Main.kt
@@ -0,0 +1,147 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+
+//    fun gradingCalculator() {
+//        //Grading calculation with when
+//        println("enter your Grade :")
+//        val grade:Int = readLine()!!.trim().toInt()
+//
+//        when (grade) {
+//            in 80..100 -> println("A+")
+//            in 50..80 -> println("B+")
+//            else -> println("Fail")
+//        }
+//    }
+//
+//    gradingCalculator()
+//
+//
+//    //calculator making
+//    fun calculator() {
+//        print("enter first number: ")
+//        val a = readln().toDouble()
+//        print("enter first number: ")
+//        val b = readln().toDouble()
+//        print("enter operator + - * / :")
+//        val operator = readln().toString()
+//
+//        val result = when (operator) {
+//            "+" -> a + b
+//            "-" -> a - b
+//            "*" -> a * b
+//            "/" -> if (b != 0.0) {
+//                a / b
+//            } else {
+//                println("can not devide by zero")
+//            }
+//
+//            else -> {
+//                println("invalid operator")
+//                return
+//            }
+//        }
+//        println(result)
+//    }
+//
+//    calculator()
+
+
+//// 1. Positive/Negative/Zero: Take an integer and print whether it is positive, negative, or zero.
+//fun positive_negative_number() {
+//    println("Enter your number: ")
+//    val number = readLine()!!.toInt()
+//
+//    if(number > 0 ) {
+//        println("Your number is positive.")
+//    } else if (number < 0) {
+//        println("your number is negative.")
+//    } else {
+//        println("Your number is zero")
+//    }
+//}
+//    positive_negative_number()
+
+////2. Determine if a number is even or odd.
+//fun negative_number() {
+//    println("Enter your number: ")
+//    val number = readLine()!!.toInt()
+//    if(number % 2 ==0 ) {
+//        println("Your number is even.")
+//    } else {
+//        println("number is odd")
+//    }
+//}
+//    negative_number()
+
+//    // 3.  Given an age, check if a person is eligible to vote (18 or older).
+//    fun vote_eligibility() {
+//        println("Enter your age: ")
+//        val personAge = readLine()!!.toInt()
+//
+//        if (personAge > 18) {
+//            println("Your age is $personAge Your are eligible for vote.")
+//        } else {
+//            println("Your age is $personAge and you are not eligible for vote.")
+//        }
+//    }
+
+//    //4. Take two numbers and print which one is larger, or if they are equal.
+//    fun largestNumber() {
+//        println("Enter you first number :")
+//        val number1 = readLine()!!.toInt()
+//        println("Enter you second number :")
+//        val number2 = readLine()!!.toInt()
+//
+//        if (number1 > number2) {
+//            println("$number1 > $number2 first number is greater than second number")
+//        } else if (number1 < number2) {
+//            println("$number1 < $number2 first number is smallest than second number")
+//        }
+//    }
+
+
+//    // 5.Given a score (0-100), print "Pass" if it's 50 or above, otherwise print "Fail."
+//    fun calculatePassScore() {
+//        println("Enter your score :")
+//        val score = readLine()!!.toInt()
+//
+//        if (score > 50) {
+//            println("pass")
+//        } else if (score < 50) {
+//            println("Fail")
+//        }
+//    }
+
+//    //6. School Grading: Input a score and assign a grade: 90+ (A), 80-89 (B), 70-79 (C), 60-69 (D), below 60 (F).
+//    fun calculateSchoolGrade() {
+//        val grade = readLine()!!.toInt()
+//
+//        if (grade >= 90) {
+//            println("A")
+//        } else if (grade >= 80) {
+//            println("B")
+//        } else if (grade >= 70) {
+//            println("C")
+//        } else if (grade >= 60) {
+//            println("D")
+//        } else if (grade < 60) {
+//            println("F")
+//        }
+//    }
+
+    //7. Age Groups: Categorize a person based on age: 0-12 (Child), 13-19 (Teenager), 20-59 (Adult), 60+ (Senior).
+    
+
+}
+
+
+
+
+
+
+
+
+
+
